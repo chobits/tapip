9322d14d1baf8459a19f9b99285dd3900fef521b
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-07-30 02:24:48 +0800

    add bridge-network topology config script

---
+++ doc/brcfg.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# TOPOLOGY:
+#
+#             br0
+#            /   \
+#        eth0     veth0
+#         /         \
+#     network       /dev/net/tun
+#                     \                     
+#                   ./net_stack
+#
+# NOTE: We view veth0, /dev/net/tun and net_stack as one interface,
+#       They should have only one mac address(eth0 mac), which will
+#       handle arp protocol right.
+#       Then eth0, br0, veth0, /dev/net/tun and net_stack can be
+#       viewed as only one interface, which is similar as the original
+#       real eth0 interface, and net_stack will become its internal
+#       TCP/IP stack!
+#
+# WARNING: This test will suspend real kernel TCP/IP stack!
+
+#create  veth0(tap)
+tunctl -b -t veth0
+
+#create bridge
+brctl addbr br0
+
+#bridge config
+brctl addif br0 eth0
+brctl addif br0 veth0
+
+# net interface up
+ifconfig eth0 0.0.0.0
+ifconfig veth0 0.0.0.0 up
+# config according to your real network environment
+ifconfig br0 10.20.133.21 netmask 255.255.255.0 broadcast 10.20.133.255 up
+
+# route config
+#route add -net 10.20.133.0 netmask 255.255.255.0 br0
+route add default gw 10.20.133.20
