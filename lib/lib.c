5436cda3382c579b53653bf01f3f3f869b9ac668
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-07-30 15:53:35 +0800

    Add prints() to lib.c
    prints(len, fmt, ...) can print solid @len size data,
    the insufficient data will be covered with space char

--- lib/lib.c
+++ lib/lib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <errno.h>
 
@@ -11,4 +12,19 @@ void perrx(char *str)
 	else
 		ferr("ERROR:%s\n", str);
 	exit(EXIT_FAILURE);
+}
+
+/* format and print mlen-max-size data (spaces will fill the buf) */
+static char *_space = "                                              ";
+void printfs(int mlen, const char *fmt, ...)
+{
+	char buf[256];
+	va_list ap;
+	int slen;
+	va_start(ap, fmt);
+	slen = vsprintf(buf, fmt, ap);
+	va_end(ap);
+	printf("%.*s", mlen, buf);
+	if (mlen > slen)
+		printf("%.*s", mlen - slen, _space);
 }
