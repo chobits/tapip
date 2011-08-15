4ce5022b72a0b222af0439905cd1d1418b8bdd4a
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-08-15 15:08:14 +0800

    Update icmp implementation
     Details:
      1. add icmp_desc structure to handle icmp input packet in common use
      2. add icmp_send() to generate icmp error message
      3. add new test script to doc/test

--- include/icmp.h
+++ include/icmp.h
@@ -10,19 +10,83 @@ struct icmp {
 			unsigned short id;	/* identifier */
 			unsigned short seq;	/* sequence number */
 		} echo;
+		unsigned int pad;
 	} icmp_un;
 	unsigned char icmp_data[0];
 } __attribute__((packed));
 
 #define icmp_id icmp_un.echo.id
 #define icmp_seq icmp_un.echo.id
+#define icmp_undata icmp_un.pad
 
 #define ICMP_HRD_SZ sizeof(struct icmp)
+#define ip2icmp(ip) ((struct icmp *)ipdata(ip))
 
+/* icmp type */
 #define ICMP_T_ECHORLY		0
+#define ICMP_T_DUMMY_1		1
+#define ICMP_T_DUMMY_2		2
 #define ICMP_T_DESTUNREACH	3
+#define ICMP_T_SOURCEQUENCH	4
 #define ICMP_T_REDIRECT		5
+#define ICMP_T_DUMMY_6		6
+#define ICMP_T_DUMMY_7		7
 #define ICMP_T_ECHOREQ		8
+#define ICMP_T_DUMMY_9		9
+#define ICMP_T_DUMMY_10		10
 #define ICMP_T_TIMEEXCEED	11
+#define ICMP_T_PARAMPROBLEM	12	/* parameter problem */
+#define ICMP_T_TIMESTAMPREQ	13
+#define ICMP_T_TIMESTAMPRLY	14
+#define ICMP_T_INFOREQ		15
+#define ICMP_T_INFORLY		16
+#define ICMP_T_ADDRMASKREQ	17
+#define ICMP_T_ADDRMASKRLY	18
+#define ICMP_T_MAXNUM		18
+
+/* icmp code of ICMP_T_DESTUNREACH type */
+#define ICMP_NET_UNREACH	0
+#define ICMP_HOST_UNREACH	1
+#define ICMP_PROTO_UNREACH	2
+#define ICMP_PORT_UNREACH	3
+#define ICMP_FRAG_NEEDED	4
+#define ICMP_SROUTE_FAILED	5	/* source route: according to ip option */
+#define ICMP_NET_UNKNOWN	6	/* RFC 1812: should use code 0 */
+#define ICMP_HOST_UNKNOWN	7
+#define ICMP_SHOST_ISOLATED	8
+#define ICMP_NET_ADMINPRO	9
+#define ICMP_HOST_ADMINPRO	10
+#define ICMP_NET_UNREACHTOS	11
+#define ICMP_HOST_UNREACHTOS	12
+#define ICMP_ADMINPRO		13	/* administratively prohibited */
+#define ICMP_PREC_VIOLATION	14	/* host precedence violation */
+#define ICMP_PREC_CUTOFF	15	/* precedence cutoff in effect */
+
+/* icmp code of ICMP_T_REDIRECT type */
+#define ICMP_REDIRECT_NET	0	/* net, obsolete(RFC 1812-5.2.7.2) */
+#define ICMP_REDIRECT_HOST	1	/* host */
+#define ICMP_REDIRECT_TOSNET	2	/* type of service and net ,obsolete */
+#define ICMP_REDIRECT_TOSHOST	3	/* type of service and host */
+
+/* icmp code of ICMP_T_TIMEEXCEED type */
+#define ICMP_EXC_TTL		0
+#define ICMP_EXC_FRAGTIME	1
+
+/* icmp handler descriptor */
+struct icmp_desc {
+	int error;
+	char *info;
+	void (*handler)(struct icmp_desc *, struct pkbuf *);
+};
+
+#define ICMP_DESC_DUMMY_ENTRY \
+{\
+	.error = 1,\
+	.info = NULL,\
+	.handler = icmp_drop_reply,\
+}
+
+#define icmp_type_error(type) icmp_table[type].error
+#define icmp_error(icmphdr) icmp_type_error((icmphdr)->icmp_type)
 
 #endif	/* icmp.h */
--- ip/icmp.c
+++ ip/icmp.c
@@ -1,80 +1,255 @@
+/*
+ * Reference:
+ *  RFC 792
+ *  RFC 1812
+ *  Linux 2.6.11.12
+ *  Xinu
+ */
 #include "netif.h"
 #include "ip.h"
 #include "icmp.h"
 #include "route.h"
 #include "lib.h"
 
+static void icmp_drop_reply(struct icmp_desc *, struct pkbuf *);
+static void icmp_dest_unreach(struct icmp_desc *, struct pkbuf *);
+static void icmp_echo_request(struct icmp_desc *, struct pkbuf *);
+static void icmp_echo_reply(struct icmp_desc *, struct pkbuf *);
+static void icmp_redirect(struct icmp_desc *, struct pkbuf *);
+
+static struct icmp_desc icmp_table[ICMP_T_MAXNUM + 1] = {
+	[ICMP_T_ECHORLY] = {
+		.error = 0,
+		.handler = icmp_echo_reply,
+	},
+	[ICMP_T_DUMMY_1] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_DUMMY_2] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_DESTUNREACH] = {
+		.error = 1,
+		.handler = icmp_dest_unreach,
+	},
+	[ICMP_T_SOURCEQUENCH] = {
+		.error = 1,
+		.info = "icmp source quench",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_REDIRECT] = {
+		.error = 1,
+		.info = "icmp redirect",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_DUMMY_6] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_DUMMY_7] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_ECHOREQ] = {
+		.error = 0,
+		.handler = icmp_echo_request,
+	},
+	[ICMP_T_DUMMY_9] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_DUMMY_10] = ICMP_DESC_DUMMY_ENTRY,
+	[ICMP_T_TIMEEXCEED] = {
+		.error = 1,
+		.info = "icmp time exceeded",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_PARAMPROBLEM] = {
+		.error = 1,
+		.info = "icmp parameter problem",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_TIMESTAMPREQ] = {
+		.error = 0,
+		.info = "icmp timestamp request",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_TIMESTAMPRLY] = {
+		.error = 0,
+		.info = "icmp timestamp rely",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_INFOREQ] = {
+		.error = 0,
+		.info = "icmp infomation request",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_INFORLY] = {
+		.error = 0,
+		.info = "icmp infomation reply",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_ADDRMASKREQ] = {
+		.error = 0,
+		.info = "icmp address mask request",
+		.handler = icmp_drop_reply,
+	},
+	[ICMP_T_ADDRMASKRLY] = {
+		.error = 0,
+		.info = "icmp address mask reply",
+		.handler = icmp_drop_reply,
+	}
+};
+
 unsigned short icmp_chksum(unsigned short *data, int size)
 {
 	return ip_chksum(data, size);
 }
 
-void icmp_in(struct pkbuf *pkb)
+static void icmp_dest_unreach(struct icmp_desc *icmp_desc, struct pkbuf *pkb)
+{
+	/* FIXME: report error to upper application layer */
+	icmpdbg("dest unreachable");
+	free_pkb(pkb);
+}
+
+static char *redirectstr[4] = {
+	[ICMP_REDIRECT_NET]	= "net redirect",
+	[ICMP_REDIRECT_HOST]	= "host redirect",
+	[ICMP_REDIRECT_TOSNET]	= "type of serice and net redirect",
+	[ICMP_REDIRECT_TOSHOST]	= "type of service and host redirect"
+};
+
+static void icmp_redirect(struct icmp_desc *icmp_desc, struct pkbuf *pkb)
 {
 	struct ip *iphdr = pkb2ip(pkb);
-	struct icmp *icmphdr = (struct icmp *)iphdr->ip_data;
-	unsigned int tmp;
-	int icmplen;
+	struct icmp *icmphdr = ip2icmp(iphdr);
 
-	icmplen = iphdr->ip_len - iphlen(iphdr);
-	icmpdbg("%d bytes", icmplen);
-	if (icmplen < ICMP_HRD_SZ) {
-		icmpdbg("icmp header is too small");
-		free_pkb(pkb);
-		return;
-	}
+	if (icmphdr->icmp_code > 4)
+		icmpdbg("Redirect code %d is error", icmphdr->icmp_code);
+	else
+		icmpdbg("from " IPFMT " %s(new nexthop "IPFMT")",
+			iphdr->ip_src, redirectstr[icmphdr->icmp_code]);
+	free_pkb(pkb);
+}
 
-	if (icmp_chksum((unsigned short *)icmphdr, icmplen) != 0) {
-		icmpdbg("icmp checksum is error");
-		free_pkb(pkb);
-		return;
-	}
-	switch (icmphdr->icmp_type) {
-	case ICMP_T_ECHOREQ:
-		icmpdbg("echo request data %d bytes", icmplen - ICMP_HRD_SZ);
-		if (icmphdr->icmp_code) {
-			icmpdbg("echo request packet corrupted");
-			free_pkb(pkb);
-			return;
-		}
-		if (icmplen < ICMP_HRD_SZ) {
-			icmpdbg("echo request packet is too small");
-			free_pkb(pkb);
-			return;
-		}
-		icmphdr->icmp_type = ICMP_T_ECHORLY;
-		/*
-		 * adjacent the checksum:
-		 * If  ~ >>> (cksum + x + 8) >>> == 0
-		 * let ~ >>> (cksum` + x ) >>> == 0
-		 * then cksum` = cksum + 8
-		 */
-		if (icmphdr->icmp_cksum > 0xffff - ICMP_T_ECHOREQ)
-			icmphdr->icmp_cksum += ICMP_T_ECHOREQ + 1;
-		else
-			icmphdr->icmp_cksum += ICMP_T_ECHOREQ;
-		tmp = iphdr->ip_src;
-		iphdr->ip_src = iphdr->ip_dst;
-		iphdr->ip_dst = tmp;
-		ip_hton(iphdr);
-		ip_send(pkb, 0);
-		break;
-	case ICMP_T_DESTUNREACH:
-		icmpdbg("destination unreachable");
-		break;
-	case ICMP_T_REDIRECT:
-		icmpdbg("redirect");
-		break;
-	case ICMP_T_ECHORLY:
-		icmpdbg("from "IPFMT" id %d seq %d ttl %d",
+static void icmp_echo_reply(struct icmp_desc *icmp_desc, struct pkbuf *pkb)
+{
+	struct ip *iphdr = pkb2ip(pkb);
+	struct icmp *icmphdr = ip2icmp(iphdr);
+	icmpdbg("from "IPFMT" id %d seq %d ttl %d",
 			ipfmt(iphdr->ip_src),
 			ntohs(icmphdr->icmp_id),
 			ntohs(icmphdr->icmp_seq),
 			iphdr->ip_ttl);
-		break;
-	default:
-		icmpdbg("unknown type");
-		free_pkb(pkb);
-		break;
-	}
+	free_pkb(pkb);
 }
+
+static void icmp_echo_request(struct icmp_desc *icmp_desc, struct pkbuf *pkb)
+{
+	struct ip *iphdr = pkb2ip(pkb);
+	struct icmp *icmphdr = ip2icmp(iphdr);
+	icmpdbg("echo request data %d bytes",
+			iphdr->ip_len - iphlen(iphdr) - ICMP_HRD_SZ);
+	if (icmphdr->icmp_code) {
+		icmpdbg("echo request packet corrupted");
+		free_pkb(pkb);
+		return;
+	}
+	icmphdr->icmp_type = ICMP_T_ECHORLY;
+	/*
+	 * adjacent the checksum:
+	 * If  ~ >>> (cksum + x + 8) >>> == 0
+	 * let ~ >>> (cksum` + x ) >>> == 0
+	 * then cksum` = cksum + 8
+	 */
+	if (icmphdr->icmp_cksum >= 0xffff - ICMP_T_ECHOREQ)
+		icmphdr->icmp_cksum += ICMP_T_ECHOREQ + 1;
+	else
+		icmphdr->icmp_cksum += ICMP_T_ECHOREQ;
+	iphdr->ip_dst = iphdr->ip_src;	/* ip_src is set by ip_send_out() */
+	ip_hton(iphdr);
+	ip_send_out(pkb);
+}
+
+static void icmp_drop_reply(struct icmp_desc *icmp_desc, struct pkbuf *pkb)
+{
+	struct ip *iphdr = pkb2ip(pkb);
+	struct icmp *icmphdr = ip2icmp(iphdr);
+	icmpdbg("icmp type %d code %d (dropped)",
+			icmphdr->icmp_type, icmphdr->icmp_code);
+	if (icmp_desc->info)
+		icmpdbg("%s", icmp_desc->info);
+	free_pkb(pkb);
+}
+
+void icmp_in(struct pkbuf *pkb)
+{
+	struct ip *iphdr = pkb2ip(pkb);
+	struct icmp *icmphdr = ip2icmp(iphdr);
+	int icmplen, type;
+	/* sanity check */
+	icmplen = iphdr->ip_len - iphlen(iphdr);
+	icmpdbg("%d bytes", icmplen);
+	if (icmplen < ICMP_HRD_SZ) {
+		icmpdbg("icmp header is too small");
+		goto drop_pkb;
+	}
+
+	if (icmp_chksum((unsigned short *)icmphdr, icmplen) != 0) {
+		icmpdbg("icmp checksum is error");
+		goto drop_pkb;
+	}
+
+	type = icmphdr->icmp_type;
+	if (type > ICMP_T_MAXNUM) {
+		icmpdbg("unknown icmp type %d code %d",
+					type, icmphdr->icmp_code);
+		goto drop_pkb;
+	}
+	/* handle icmp type */
+	icmp_table[type].handler(&icmp_table[type], pkb);
+	return;
+drop_pkb:
+	free_pkb(pkb);
+}
+
+/* NOTE: icmp dont drop @ipkb */
+void icmp_send(unsigned char type, unsigned char code,
+		unsigned int data, struct pkbuf *pkb_in)
+{
+	struct pkbuf *pkb;
+	struct ip *iphdr = pkb2ip(pkb_in);
+	struct icmp *icmphdr;
+	int paylen = ntohs(iphdr->ip_len);	/* icmp payload length */
+	if (paylen < iphlen(iphdr) + 8)
+		return;
+	/*
+	 * RFC 1812 Section 4.3.2.7 for sanity check
+	 * An ICMP error message MUST NOT be sent as the result of receiving:
+	 * 1. A packet sent as a Link Layer broadcast or multicast
+	 * 2. A packet destined to an IP broadcast or IP multicast address
+	 *[3] A packet whose source address has a network prefix of zero or is an
+	 *      invalid source address (as defined in Section [5.3.7])
+	 * 4. Any fragment of a datagram other then the first fragment (i.e., a
+	 *      packet for which the fragment offset in the IP header is nonzero).
+	 * 5. An ICMP error message
+	 */
+	if (pkb_in->pk_type != PKT_LOCALHOST)
+		return;
+	if (MULTICAST(iphdr->ip_dst) || BROADCAST(iphdr->ip_dst))
+		return;
+	if (iphdr->ip_fragoff & htons(IP_FRAG_OFF))
+		return;
+
+	if (icmp_type_error(type) && iphdr->ip_pro == IP_P_ICMP) {
+		icmphdr = ip2icmp(iphdr);
+		if (icmphdr->icmp_type > ICMP_T_MAXNUM || icmp_error(icmphdr))
+			return;
+	}
+	/* build icmp packet and send */
+	/* ip packet size must be smaller than 576 bytes */
+	if (IP_HRD_SZ + ICMP_HRD_SZ + paylen > 576)
+		paylen = 576 - IP_HRD_SZ - ICMP_HRD_SZ;
+	pkb = alloc_pkb(ETH_HRD_SZ + IP_HRD_SZ + ICMP_HRD_SZ + paylen);
+	icmphdr = (struct icmp *)(pkb2ip(pkb)->ip_data);
+	icmphdr->icmp_type = type;
+	icmphdr->icmp_code = code;
+	icmphdr->icmp_cksum = 0;
+	icmphdr->icmp_undata = data;
+	memcpy(icmphdr->icmp_data, (unsigned char *)iphdr, paylen);
+	icmphdr->icmp_cksum =
+		icmp_chksum((unsigned short *)icmphdr, ICMP_HRD_SZ + paylen);
+	icmpdbg("to "IPFMT"(payload %d) [type %d code %d]\n",
+		ipfmt(iphdr->ip_src), paylen, type, code);
+	ip_send_info(pkb, 0, IP_HRD_SZ + ICMP_HRD_SZ + paylen,
+						0, IP_P_ICMP, iphdr->ip_src);
+}
+
