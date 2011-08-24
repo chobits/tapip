bd335842dffe4a5cf19efd60d50c68a9427343a7
Xiaochen Wang <wangxiaochen0@gmail.com>
2011-08-25 00:07:34 +0800

    add hash list support

--- include/list.h
+++ include/list.h
@@ -7,6 +7,7 @@
 #define NULL ((void *)0)
 #endif
 
+/* list head */
 struct list_head {
 	struct list_head *prev, *next;
 };
@@ -48,6 +49,13 @@ static _inline void list_del(struct list_head *list)
 	list->next = NULL;
 }
 
+static _inline void list_del_init(struct list_head *list)
+{
+	__list_del(list->prev, list->next);
+	list->prev = list;
+	list->next = list;
+}
+
 #define LIST_HEAD(name)\
 	struct list_head name = { &name, &name };
 
@@ -74,5 +82,82 @@ static _inline void list_del(struct list_head *list)
 	for (entry = list_last_entry(head, typeof(*entry), member);\
 		&entry->member != (head);\
 		entry = list_last_entry(&entry->member, typeof(*entry), member))
+
+/* hash list head */
+struct hlist_head {
+	struct hlist_node *first;
+};
+
+struct hlist_node {
+	struct hlist_node *next;
+	struct hlist_node **pprev;
+};
+
+#define hlist_empty(head) (((struct hlist_head *)(head))->first == NULL)
+
+static _inline void hlist_head_init(struct hlist_head *head)
+{
+	head->first = NULL;
+}
+
+static _inline void hlist_node_init(struct hlist_node *node)
+{
+	node->next = NULL;
+	node->pprev = NULL;
+}
+
+static _inline void __hlist_del(struct hlist_node *n)
+{
+	*n->pprev = n->next;
+	if (n->next)
+		n->next->pprev = n->pprev;
+}
+
+static _inline void hlist_del(struct hlist_node *n)
+{
+	__hlist_del(n);
+	n->next = NULL;
+	n->pprev = NULL;
+}
+
+static _inline void hlist_add_head(struct hlist_node *n, struct hlist_head *h)
+{
+	n->next = h->first;
+	n->pprev = &h->first;
+	if (h->first)
+		h->first->pprev = &n->next;
+	h->first = n;
+}
+
+/* add @n before @next */
+static _inline void hlist_add_before(struct hlist_node *n, struct hlist_node *next)
+{
+	n->next = next;
+	n->pprev = next->pprev;
+	*next->pprev = n;
+	next->pprev = &n->next;
+}
+
+/* add @next after @n */
+static _inline void hlist_add_after(struct hlist_node *n, struct hlist_node *next)
+{
+	next->next = n->next;
+	next->pprev = &n->next;
+	if (n->next)
+		n->next->pprev = &next->next;
+	n->next = next;
+}
+
+#define hlist_entry(ptr, type, member) list_entry(ptr, type, member)
+/* non-node implementation */
+#define hlist_for_each_entry2(entry, head, member)\
+	for (entry = ((head)->first) ? hlist_entry((head)->first, typeof(*entry), member) : NULL;\
+		entry;\
+		entry = (entry->member.next) ? hlist_entry(entry->member.next, typeof(*entry), member) : NULL)
+
+#define hlist_for_each_entry(entry, node, head, member)\
+	for (node = (head)->first;\
+		node && (entry = hlist_entry(node, typeof(*entry), member));\
+		node = node->next)
 
 #endif	/* list.h */
